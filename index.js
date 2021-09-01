dac116f6685055992439f5233d0e0ceab7cdc457
Patrick Dinehart <pxd4962@rit.edu>
2021-09-01 15:19:08 -0400

    test

---
+++ index.js
@@ -0,0 +1,36 @@
+const { request, response } = require('express');
+const express = require('express');
+const Datastore = require('nedb');
+const app = express();
+app.listen(8000, () => {
+    return console.log("Listening at port 8000");
+});
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/post', (request, response) => {
+    console.log("I got a post request");
+    const data = request.body;
+    database.remove({}, { multi: true }, function (err, numRemoved) {
+    });
+    database.insert(data);
+    response.json({
+        status: 'success!'
+    });
+});
+
+app.get('/get', (request, response) => {
+    console.log("I got a get request");
+    database.find({}, { teams: 1, results: 1, _id: 0 }, function (err, data) {
+        if(err) {
+            response.end();
+            return;
+        }
+        response.json({
+            body: data[0]
+        });
+    });
+});
